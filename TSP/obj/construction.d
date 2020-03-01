3bcb6ad6b00a9907959bb3cbd7bb23a4f88d377e
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-02-29 21:49:31 -0300

    Included 10 repetitions

--- TSP/obj/construction.d
+++ TSP/obj/construction.d
@@ -1,2 +1,2 @@
 obj/construction.o: src/construction.cpp src/construction.h src/solution.h \
- src/input.h src/colormod.h
+  src/input.h src/colormod.h
--- TSP/src/main.cpp
+++ TSP/src/main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 int main(int argc, char** argv) {
     int seed = time(0);
-    srand(seed);
     //srand(1582915991);
 
     Input in(argc, argv);
@@ -28,19 +27,20 @@ int main(int argc, char** argv) {
         R.push_back(R[i-1]+0.01);
     // GILSRVND parameters
 
-    clock_t beginC = clock();
-    // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    sol = ls.GILSRVND(Imax,Iils,R);
-    // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    clock_t endC = clock();
+    
+    for(int i=1; i <=10; i++ ){
+        seed++;
+        srand(seed);
+        clock_t beginC = clock();
+        sol = ls.GILSRVND(Imax,Iils,R);
+        clock_t endC = clock();
+        double elapsed_secs = double(endC - beginC) / CLOCKS_PER_SEC;
+        printf("%.*s", strlen(argv[1])-14, argv[1] + 10);
+        cout<<","<<elapsed_secs<<","<<sol.costValue<<","<<seed<<endl;
+    }
+    
 
-    // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
-    // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
-    double elapsed_secs = double(endC - beginC) / CLOCKS_PER_SEC;
-
-    cout<<"{";
-    printf("%.*s", strlen(argv[1])-14, argv[1] + 10);
-    cout<<","<<elapsed_secs<<","<<sol.costValue<<","<<seed<<"}"<<endl;
+    
 
     return 0;
 }
