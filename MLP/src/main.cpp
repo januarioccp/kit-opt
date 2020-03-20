5de3d9dfc0c6ea65742f37fe245b89a22903beb8
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-03-19 21:05:51 -0300

    simple test

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -17,30 +17,33 @@ int main(int argc, char** argv) {
 
     Input in(argc, argv);
     in.problemSet(1);
-    Solution sol(&in);
-    LocalSearch ls(&in);
 
-    // BEGIN GILSRVND parameters
-    int Imax = 50;
-    int Iils = (in.dimensionGet() >= 150) ? in.dimensionGet()/2 : in.dimensionGet();
-    vector<double> R;
-    R.push_back(0.00);
-    for(int i=1; i <= 25; i++)
-        R.push_back(R[i-1]+0.01);
-    // END GILSRVND parameters
+    int * format = new int[8];
+    delete [] format;
+    //Solution sol(&in);
+    // LocalSearch ls(&in);
 
-    for(int i=1; i <=1; i++ ){
-        seed++;
-        srand(seed);
-        clock_t beginC = clock();
-        sol = ls.GILSRVND(Imax,Iils,R);
-        clock_t endC = clock();
-        double elapsed_secs = double(endC - beginC) / CLOCKS_PER_SEC;
-        printf("%.*s", int(strlen(argv[1]))-14, argv[1] + 10);
-        cout<<","<<elapsed_secs<<",";
-        cout<<sol.costValueMLP;
-        cout<<","<<seed<<endl;
-    }
+    // // BEGIN GILSRVND parameters
+    // int Imax = 50;
+    // int Iils = (in.dimensionGet() >= 150) ? in.dimensionGet()/2 : in.dimensionGet();
+    // vector<double> R;
+    // R.push_back(0.00);
+    // for(int i=1; i <= 25; i++)
+    //     R.push_back(R[i-1]+0.01);
+    // // END GILSRVND parameters
+
+    // for(int i=1; i <=1; i++ ){
+    //     seed++;
+    //     srand(seed);
+    //     clock_t beginC = clock();
+    //     sol = ls.GILSRVND(Imax,Iils,R);
+    //     clock_t endC = clock();
+    //     double elapsed_secs = double(endC - beginC) / CLOCKS_PER_SEC;
+    //     printf("%.*s", int(strlen(argv[1]))-14, argv[1] + 10);
+    //     cout<<","<<elapsed_secs<<",";
+    //     cout<<sol.costValueMLP;
+    //     cout<<","<<seed<<endl;
+    // }
 
     return 0;
 }
