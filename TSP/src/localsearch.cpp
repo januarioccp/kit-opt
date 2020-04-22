79f3450d0ed7dec6ecd7cb118c28f5a82899b71b
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-04-22 19:01:10 -0300

    more neighborhoods

--- TSP/src/localsearch.cpp
+++ TSP/src/localsearch.cpp
@@ -40,8 +40,8 @@ Solution LocalSearch::GILSRVND(int Imax, int Iils, vector<double> &R)
     reset();
 
     alpha = randomValue(R);
-    // c->trivial(s);
-    c->constructiveProcedure(s,alpha);
+    c->trivial(s);
+    // c->constructiveProcedure(s,alpha);
     cout << fixed << setprecision(2) << s->costValueTSP;
     cout << " 0" << endl;
     for (int i = 0; i < s->location.size() - 1; i++)
